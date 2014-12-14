e7720c8d93be55fcbd7edad9eb9611cc84e5c9da
0xD34D <clark@scheffsblend.com>
2014-12-14 21:23:05 +0100

    Gesture Anywhere [1/2]
    Change-Id: Ie15c4939af7cfc6ed5dbca5addadffc0b4bfb764
    
    Missing from Gesture Anywhere commit
    
    Change-Id: I684595fd01e518857071cd0402ded7e125fa869b
    
    Conflicts:
    	packages/SystemUI/res/values/cr_colors.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -19,6 +19,9 @@ package com.android.systemui.statusbar.phone;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.animation.TimeInterpolator;
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
 import android.annotation.NonNull;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
@@ -882,6 +885,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // ================================================================================
     // Constructing the view
     // ================================================================================
+    @ChaosLab(name="GestureAnywhere", classification=Classification.CHANGE_CODE)
     protected PhoneStatusBarView makeStatusBarView() {
         final Context context = mContext;
 
@@ -973,6 +977,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                         checkUserAutohide(v, event);
                         return false;
                     }});
+
+        addGestureAnywhereView();
+
             }
         } catch (RemoteException ex) {
             // no window manager? good luck with that
