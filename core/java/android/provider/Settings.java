9ee7beb3e77fb1af4650de94241f684c6e69d758
cphelps76 <cphelps76@droidconcepts.com>
2015-11-22 13:53:07 +0100

    BatteryBar [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3561,6 +3561,31 @@ public final class Settings {
                 "volume_keys_control_ring_stream";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_STYLE = "statusbar_battery_bar_style";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
         /**
          * Statusbar Bliss logo
          * @hide
