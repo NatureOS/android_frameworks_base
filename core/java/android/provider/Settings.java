bd41ab37eb4d8192b50f6be011c6f586299d9b0b
풀그린(Fullgreen) <abin030187@naver.com>
2016-03-27 07:23:03 +0900

    Toast icon switch [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3543,6 +3543,13 @@ public final class Settings {
                 "volume_keys_control_ring_stream";
 
         /**
+         * Toast icon
+         *
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
