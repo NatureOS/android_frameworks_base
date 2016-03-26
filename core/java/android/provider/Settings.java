9f306abe2836ba6c6ebefe125ee2484664c74874
풀그린(Fullgreen) <abin030187@naver.com>
2016-03-27 07:39:15 +0900

    Wakelock Blocker [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3823,6 +3823,18 @@ public final class Settings {
          */
         public static final String STATUS_BAR_CARRIER_COLOR = "status_bar_carrier_color";
 
+	/**
+	 * Enable blocking wakelock
+	 * @hide
+	 */
+	public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+	/**
+	 * List of wakelock blocks selected
+	 * @hide
+	 */
+	public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
