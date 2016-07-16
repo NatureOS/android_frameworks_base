d526a092efac6449c3d6bae31ab491560046eee4
풀그린(Fullgreen) <abin030187@naver.com>
2016-07-16 10:54:47 +0900

    FWB: Gesture Anywhere - bring it back [1/2]

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -19,6 +19,8 @@ package com.android.systemui.statusbar.phone;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.annotation.ChaosLab;
+import android.annotation.ChaosLab.Classification;
 import android.annotation.NonNull;
 import android.app.ActivityManager;
 import android.app.ActivityManagerNative;
@@ -917,6 +919,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     // ================================================================================
     // Constructing the view
     // ================================================================================
+    @ChaosLab(name="GestureAnywhere", classification=Classification.CHANGE_CODE)
     protected PhoneStatusBarView makeStatusBarView() {
         final Context context = mContext;
 
@@ -1018,7 +1021,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 		
 		addSidebarView();    
-		
+
+
+        addGestureAnywhereView();
+
         if (mNavigationBarView == null) {
             mAssistManager.onConfigurationChanged();
         }
