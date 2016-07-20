51b8cc129b775500e5c603e88d96db32abf1b0dd
풀그린(Fullgreen) <abin030187@naver.com>
2016-07-21 00:08:32 +0900

    Revert "Navbar on/off and custom dimensions (1/2)"
    This reverts commit 8cf68be6e72c7debef113d59470d91c3f469d3a3.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4192,26 +4192,6 @@ public final class Settings {
         @ChaosLab(name="GestureAnywhere", classification=Classification.NEW_FIELD)
         public static final String GESTURE_ANYWHERE_SHOW_TRIGGER = "gesture_anywhere_show_trigger";
 
-	    /** Wheither navigation bar is enabled or not
-	     * @hide
-	     */
-	    public static final String NAVIGATION_BAR_SHOW = "navigation_bar_show";
-
-	    /** Navbar height
-         * @hide
-         */
-	    public static final String NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
-
-	    /** Navbar height in landscape
-         * @hide
-         */
-	    public static final String NAVIGATION_BAR_HEIGHT_LANDSCAPE = "navigation_bar_height_landscape";
-
-	    /** Navbar width
-         * @hide
-         */
-	    public static final String NAVIGATION_BAR_WIDTH = "navigation_bar_width";
-
         /**
          * Navigation bar button color
          * @hide
